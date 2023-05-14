73897ec83313f6416ae5e16d8a0034ea2779fbe9
mariomunooz <mario.munoz01@estudiant.upf.edu>
2023-05-14 04:15:03 +0200

    all

--- Imgur/generate_url.py
+++ Imgur/generate_url.py
@@ -27,8 +27,35 @@ def uploadToBlobStorage2(image, extension):
     blob_service_client = BlobServiceClient.from_connection_string(connection_string)
     blob_client = blob_service_client.get_blob_client(container= container_name, blob= f'image_{current_time}{extension}')
 
-    print(type(image))
-
     blob_client.upload_blob(image)
 
     return blob_client.url
+
+def get_num_of_images_stored():
+
+    # Initialize a BlobServiceClient object with your storage account connection string
+    blob_service_client = BlobServiceClient.from_connection_string(connection_string)
+
+    # Get a reference to the container that you want to count the blobs in
+    container_client = blob_service_client.get_container_client(container_name)
+
+    # Get the list of all blobs in the container
+    blobs_list = container_client.list_blobs()
+
+    # Count the number of blobs in the container
+    count = sum(1 for _ in blobs_list)
+
+    return count
+
+def get_urls_of_images_stored():
+    blob_service_client = BlobServiceClient.from_connection_string(connection_string)
+    container_client = blob_service_client.get_container_client(container_name)
+
+    blob_urls = []
+    blobs = container_client.list_blobs()
+    for blob in blobs:
+        blob_url = container_client.url + '/' + blob.name
+        print(blob_url)
+        blob_urls.append(blob_url)
+
+    return blob_urls
--- app.py
+++ app.py
@@ -1,8 +1,19 @@
 import streamlit as st
 from PIL import Image
 from Imgur import generate_url as gu
-
+import json
 import os
+import requests
+from streamlit_lottie import st_lottie
+import time
+
+from api_interaction import api_main as apim
+
+def load_lottieurl(url):
+    r = requests.get(url)
+    if r.status_code != 200:
+        return None
+    return r.json()
 
 # set the title of the web app
 st.title("Real State Marketplace Recommender")
@@ -18,21 +29,35 @@ house to get started. You can relax and let us take care of the rest. Our team o
 recognition technology to analyze the visual features of your property, and provide personalized recommendations 
 based on those features. """)
 
-
-
+st.write("""\n\n**Upload 4 Images of your house**""")
 # Create a button for users to upload photos
 uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
+
+
+number_of_images_stored = gu.get_num_of_images_stored()
+
 
 # Display the uploaded photo
 if uploaded_file is not None:
     img = uploaded_file.read()
 
-    image = Image.open(uploaded_file)
-
-    st.image(image, caption='Uploaded image')
+    image_url = gu.uploadToBlobStorage2(img, os.path.splitext(uploaded_file.name)[-1] )
 
 
+while number_of_images_stored < 3:
+    time.sleep(1)
 
-    st.write(f'{gu.uploadToBlobStorage2(img, os.path.splitext(uploaded_file.name)[-1] )}')
+
+st_lottie( load_lottieurl("https://assets1.lottiefiles.com/packages/lf20_lp7qD9RDx1.json"), height=200, )
+
+st.write(f'{gu.get_urls_of_images_stored()}')
+
+st.write(apim.create_estate_description(gu.get_urls_of_images_stored()))
+
+
+
+
+
+
 
 
