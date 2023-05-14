636f0421917069421418caca5f0d1c573f0f476e
mariomunooz <72894032+mariomunooz@users.noreply.github.com>
2023-05-14 23:34:07 +0200

    Update generate_url.py

--- Imgur/generate_url.py
+++ Imgur/generate_url.py
@@ -1,10 +1,10 @@
 from azure.storage.blob import BlobServiceClient
 from datetime import datetime
 
-storage_account_key = "9ySS1QeMCWCmxhQHynGjmq0fAZ5Nm8S1xTsc1uAA+o+AIxwoZc2qvzxlt65O4HqAgcfAYOdEA/W8+AStbBzmRQ=="
-storage_account_name = "hackupc"
-connection_string = "DefaultEndpointsProtocol=https;AccountName=hackupc;AccountKey=3lvbS2yJ7x4k5T8buW6fkOu9CCQgToEco100+3yt2mIC9QeIjSvmFlDWPlkqwhcbg3Bec6GIACG8+AStVRnbgA==;EndpointSuffix=core.windows.net"
-container_name = "images"
+storage_account_key = st.secrets["storage_account_key"]
+storage_account_name = st.secrets["storage_account_name"]
+connection_string = st.secrets["connection_string"]
+container_name = st.secrets["container_name"]
 
 def uploadToBlobStorage(file_path, file_name):
     blob_service_client = BlobServiceClient.from_connection_string(connection_string)
